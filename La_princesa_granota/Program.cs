e408c75ce89c689dcb04182503f9f74ce6d38ae7
Arnau Brugat <abrugat@cendrassos.net>
2024-12-20 16:44:46 +0100

    ArnauBrugat

--- La_princesa_granota/Program.cs
+++ La_princesa_granota/Program.cs
@@ -9,57 +9,64 @@ class Program
     private static Window _finestra = null!;
     private static Cavaller _cavaller = null!;
     private static List<Granota> _granotes = new();
+    private static Image PilotaOr = new Image("BalonOro.jpg");
     static void Main()
     {
         Application.Run(() =>
         {
             _finestra = new Window("La finestra", (800, 600));
             _finestra.MoveToCenter();
-            _cavaller = new Cavaller(10,10);
+            _cavaller = new Cavaller(10, 10);
+
             bool princesa = true;
             for (int i = 0; i < GranotesInicials; i++)
             {
                 _granotes.Add(new Granota(princesa));
                 princesa = false;
             }
-            
-            var loop = GameLoop.Create(_finestra.Graphics, OnUpdate);
-            loop.Start();
-        });
-    }
-    private static void OnUpdate(GraphicsContext gfx, float dt)
-    {
-        var rectangleFinestra = new Rectangle(0, 0, _finestra.Width, _finestra.Height);
-        var rectangleGranota = new Rectangle(0, 0, _finestra.Width, _finestra.Height);
-        _cavaller.Mou(rectangleFinestra);
-        Granota eliminar = null;
-
-        foreach (var granota in _granotes)
-        {
-            if (_cavaller.HaCapturatUnaGranota(granota))
+            bool gameOver = false;
+            var loop = GameLoop.Create(_finestra.Graphics, (gfx, dt) =>
             {
-                if (granota.EsPrincesa)
+                var rectangleFinestra = new Rectangle(0, 0, _finestra.Width, _finestra.Height);
+                _cavaller.Mou(rectangleFinestra);
+                Granota eliminar = null;
+                if (gameOver)
                 {
-                    gfx.Clear();
+                    gfx.DrawImage(PilotaOr, rectangleFinestra);
+                    return;
                 }
-                else
+                foreach (var granota in _granotes)
                 {
-                    eliminar = granota;
+                    if (_cavaller.HaCapturatUnaGranota(granota))
+                    {
+                        if (granota.EsPrincesa)
+                        {
+                            gameOver = true;
+                        }
+                        else
+                        {
+                            eliminar = granota;
+                        }
+                    }
                 }
-            }
-            
-        }
-        if (eliminar!= null)
-        {
-            _granotes.Remove(eliminar);
-        }
-        gfx.Clear(Color.Blue);
+                if (eliminar != null)
+                {
+                    _granotes.Remove(eliminar);
+                    _granotes.Add(new Granota(false));
+                    _granotes.Add(new Granota(false));
+                }
+                gfx.Clear(Color.LightGray);
+                _cavaller.Pinta(gfx);
+                foreach (var granota in _granotes)
+                {
+                    granota.Pinta(gfx);
+                }
+                
+            });
 
-        _cavaller.Pinta(gfx);
-        foreach (var granota in _granotes)
-        {
-            granota.Pinta(gfx);
-        }
+            loop.Start();
+            
+        });
         
     }
 }
